438edea9021876c15a8ce7b939300c6b561bfee1
Jay Modi <jaymodicr12@gmail.com>
2025-08-17 20:45:33 +0530

    added global error handler ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import express from "express";
+import globalErrorHandler from "./middlewares/globalErrorHandler.ts";
+
+// import createHttpError from "http-errors";
 
 const app = express();
 
 //routes
 //http methods get, post, put, patch, delete
 app.get("/", (req, res) => {
-    res.json({message: "welcome to masterapi"})
+
+    res.json({ message: "welcome to masterapi" });
 });
+
+app.use(globalErrorHandler)
 
 export default app;
---
+++ src/middlewares/globalErrorHandler.ts
@@ -0,0 +1,20 @@
+import type { Request, Response } from "express";
+import type { HttpError } from "http-errors";
+import { config } from "../config/config.ts";
+
+// global error handler
+const globalErrorHandler = (
+    error: HttpError,
+    req: Request,
+    res: Response,
+    // next: NextFunction
+) => {
+    const statusCode = error.statusCode || 500;
+
+    return res.status(statusCode).json({
+        message: error.message,
+        errorStack: config.env === "development" ? error.stack : "",
+    });
+};
+
+export default globalErrorHandler;
