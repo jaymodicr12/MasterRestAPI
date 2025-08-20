936dd671d3b483322a72afd2b2016447cfa81bc9
Jay Modi <jaymodicr12@gmail.com>
2025-08-20 23:50:45 +0530

    user register api commit

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler.ts";
+import userRouter from "./user/userRouter.ts";
 
 // import createHttpError from "http-errors";
 
@@ -8,10 +9,11 @@ const app = express();
 //routes
 //http methods get, post, put, patch, delete
 app.get("/", (req, res) => {
-
     res.json({ message: "welcome to masterapi" });
 });
 
-app.use(globalErrorHandler)
+app.use("/api/users", userRouter);
+
+app.use(globalErrorHandler);
 
 export default app;
--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,4 +1,4 @@
-import type { Request, Response } from "express";
+import type { Request, Response , NextFunction} from "express";
 import type { HttpError } from "http-errors";
 import { config } from "../config/config.ts";
 
@@ -7,7 +7,7 @@ const globalErrorHandler = (
     error: HttpError,
     req: Request,
     res: Response,
-    // next: NextFunction
+    next: NextFunction
 ) => {
     const statusCode = error.statusCode || 500;
 
