4b04893df95e8db90ce6ced842055589b2d26863
Jay Modi <jaymodicr12@gmail.com>
2025-08-21 01:15:34 +0530

    lol

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,27 @@
 import express from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler.ts";
 import userRouter from "./user/userRouter.ts";
-
-// import createHttpError from "http-errors";
+import createHttpError from "http-errors";
 
 const app = express();
 
-//routes
-//http methods get, post, put, patch, delete
+// enable JSON body parsing
+app.use(express.json());
+
+// root route
 app.get("/", (req, res) => {
-    res.json({ message: "welcome to masterapi" });
+  res.json({ message: "welcome to masterapi" });
 });
 
+// user routes
 app.use("/api/users", userRouter);
 
+// 404 handler
+app.use((req, res, next) => {
+  next(createHttpError(404, "Route not found"));
+});
+
+// global error handler (last)
 app.use(globalErrorHandler);
 
 export default app;
--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,9 +1,19 @@
 import type { Request, Response, NextFunction } from "express";
+import createHttpError from "http-errors";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
-    res.json({
-        message: "User Registered !",
-    });
+    const { name, email, password } = req.body;
+
+    // Validation
+    if (!name || !email || !password) {
+        const error = createHttpError(400, "All fields are required !");
+        return next(error);
+    }
+    // Process
+
+    // Response
+
+    res.json({ message: "User Registered !" });
 };
 
 export { createUser };
